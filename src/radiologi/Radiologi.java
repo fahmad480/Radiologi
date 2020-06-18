55f2f2ae9dde834176719c606ebba8a82a2cd55c
Faraaz Ahmad <33687170+zFz0000@users.noreply.github.com>
2020-06-18 10:35:17 +0700

    Update Radiologi.java

--- src/radiologi/Radiologi.java
+++ src/radiologi/Radiologi.java
@@ -16,6 +16,7 @@ public class Radiologi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
